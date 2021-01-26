19e2ff94da59e9ef85b4e7c9cfcd58ba26b93d46
小林悠希 <ore04110214@gmail.com>
2021-01-26 18:24:05 +0900

    js/main.jsのtopBtnをtop_btnに変更した

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(function(){
   
-  var topBtn = $("#page-top");
+  var top_btn = $("#page-top");
   
-  topBtn.click(function(e){
+  top_btn.click(function(e){
     e.preventDefault();
     var position = 0;
     $("html,body").animate({scrollTop:position});
